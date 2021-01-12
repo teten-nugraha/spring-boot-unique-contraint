29b2455ff4e52d3562c03525bc3b98d4b7b8fbe9
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-01-12 15:15:34 +0700

    add for valid handle exception

--- src/main/java/id/ten/checkuniquecontraint/exception/ControllerAdviceException.java
+++ src/main/java/id/ten/checkuniquecontraint/exception/ControllerAdviceException.java
@@ -1,15 +1,21 @@
 package id.ten.checkuniquecontraint.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ControllerAdviceException extends ResponseEntityExceptionHandler {
@@ -23,6 +29,29 @@ public class ControllerAdviceException extends ResponseEntityExceptionHandler {
         body.put("message", "Student Not Found");
 
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    // error handle for @Valid
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status, WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
+
+        //Get all errors
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(x -> x.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, headers, status);
+
     }
 
 }
