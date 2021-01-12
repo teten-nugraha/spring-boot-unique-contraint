514f25c83afb871ef88c483c5699c32f4c6b10e2
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-01-12 12:02:03 +0700

    first

---
+++ src/main/java/id/ten/checkuniquecontraint/CheckUniqueContraintApplication.java
@@ -0,0 +1,13 @@
+package id.ten.checkuniquecontraint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckUniqueContraintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckUniqueContraintApplication.class, args);
+	}
+
+}
